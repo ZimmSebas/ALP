aad2472cd31c15cf32f81cc4bf42e1368e5961ed
gpaoloni <paolonigianfranco@gmail.com>
2019-09-19 19:08:33 -0300

    shift and subst implemented

--- TP2/Untyped.hs
+++ TP2/Untyped.hs
@@ -31,12 +31,25 @@ conversion' (Abs str t) state = let s = str : state
 -------------------------------
 
 shift :: Term -> Int -> Int -> Term
-shift = undefined
-  
-  
-subst :: Term -> Term -> Int -> Term
-subst = undefined
+shift (Bound k) c d | k >= c    = Bound $ k+d
+                    | otherwise = Bound k
+shift (Free x)  c d = Free x
+shift (t :@: u) c d = (shift t c d) :@: (shift u c d)
+shift (Lam t)   c d = Lam (shift t c d)
 
+subst :: Term -> Term -> Int -> Term
+subst t1 t2 i = subst' t1 t2 i 0
+
+subst' :: Term -> Term -> Int -> Int -> Term
+subst' t1 t2 i j = case t1 of
+    (Bound k) -> substBound k t2 i j
+    (v :@: u) -> (subst' v t2 i j) :@: (subst' u t2 i j)
+    (Lam t)   -> Lam (subst' t t2 i (j+1))
+
+substBound :: Int -> Term -> Int -> Int -> Term
+substBound k t2 i j | k == i = shift t2 k j
+                    | k >  i = Bound (k-1)
+                    | k <  i = Bound k
 
 eval :: NameEnv Term -> Term -> Term
 eval = undefined
